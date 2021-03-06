a244ee71951033716d56cbeb2b1ddbd86e6a9def
gabrielcastellani <gabrielcastellanioliveira@gmail.com>
2021-03-05 22:07:15 -0300

    Example service

---
+++ lib/services/userManager.dart
@@ -0,0 +1,19 @@
+import 'package:app_estacionamento/helpers/firebase_errors.dart';
+import 'package:app_estacionamento/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+
+class UserManager {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
+    try {
+      final AuthResult result = await auth.signInWithEmailAndPassword(
+          email: user.email, password: user.password);
+
+      onSuccess();
+    } on PlatformException catch (e) {
+      onFail(getErrorString(e.code));
+    }
+  }
+}
