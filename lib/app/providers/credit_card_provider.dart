16d1afab0552d00fc2a58ff5a9b28e417593da0b
gabrielcastellani <gabrielcastellanioliveira@gmail.com>
2021-05-16 18:03:55 -0300

    Criado mensagem para o usuário escolher se deseja cadastrar o cartão para futuros pagamentos

--- lib/app/providers/credit_card_provider.dart
+++ lib/app/providers/credit_card_provider.dart
@@ -2,17 +2,20 @@ import 'package:app_estacionamento/app/models/credit_card.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 class CreditCardProvider extends ChangeNotifier {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  final String COLLECTION_NAME = 'creditCards';
 
   Future<CreditCardModel> getCreditCardByCurrentUser() async {
     var uid = _firebaseAuth.currentUser.uid;
     var creditCard = CreditCardModel();
 
     final QuerySnapshot query = await _firebaseFirestore
-        .collection('creditCards')
+        .collection(COLLECTION_NAME)
         .where('uid', isEqualTo: uid)
         .get();
 
@@ -23,5 +26,23 @@ class CreditCardProvider extends ChangeNotifier {
     }
 
     return creditCard;
+  }
+
+  Future<void> createCreditCardForCurrentUser(
+      {CreditCardModel creditCardModel,
+      Function onSucess,
+      Function onFail}) async {
+    try {
+      var uid = _firebaseAuth.currentUser.uid;
+      creditCardModel.uid = uid;
+
+      await _firebaseFirestore
+          .collection(COLLECTION_NAME)
+          .add(creditCardModel.toJson());
+
+      onSucess();
+    } on PlatformException catch (e) {
+      onFail(e.message);
+    }
   }
 }
